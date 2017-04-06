13185e2624d57362025296b1e04ca73bbf206c13
InfinityQX70s <mazumisha@gmail.com>
2017-04-07 00:12:21 +0300

    Added jsoup lib for html parsing

---
+++ core/src/main/java/com/spring/sample/Main.java
@@ -0,0 +1,46 @@
+package com.spring.sample;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
+/**
+ * Created by Mikhail on 07.04.2017.
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Document document = Jsoup.connect("url").get();
+        //get film name with year
+        //TODO:: cut the year from the end of film title
+        document.select("h1[itemprop=name]").first().text();
+        //getting year value
+        document.select("span#titleYear").first().text();
+        //getting original title
+        document.select("div.originalTitle").first().text();
+        //getting content rating
+        document.select("meta[itemprop=contentRating]").attr("content");
+        //obtain film duration
+        document.select("time[itemprop=duration]").first().text();
+        //for each through the list of genres
+        //TODO:: for each from the list
+        document.select("span[itemprop=genre]").first().text();
+        //get date of film publishing
+        //TODO:: check got two values
+        document.select("meta[itemprop=datePublished]").attr("content");
+        //getting poster image
+        document.select("img[itemprop=image]").first().attr("src");
+        //get film description
+        document.select("div[itemprop=description].summary_text").first().text();
+        //get film director
+        document.select("span[itemprop=director]").first().text();
+        //get creators list
+        //TODO:: for each check cut the begin of the line 'Writers:'
+        document.select("div.credit_summary_item").get(1).text();
+        //get actors list
+        //TODO:: foreach iterate (split by ',')
+        document.select("span[itemprop=actors]").text();
+
+    }
+}
