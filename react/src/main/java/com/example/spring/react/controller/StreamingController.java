839122987af8d547d4bbe321e3b6ce808f5c7529
mmazurkevich <mazumisha@gmail.com>
2018-03-28 22:55:46 +0300

    Add link to reactore core guideline

--- react/src/main/java/com/example/spring/react/controller/StreamingController.java
+++ react/src/main/java/com/example/spring/react/controller/StreamingController.java
@@ -1,11 +1,13 @@
 package com.example.spring.react.controller;
 
+import com.example.spring.react.model.Message;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 import java.time.Duration;
+import java.time.LocalDateTime;
 
 @RestController
 public class StreamingController {
@@ -13,7 +15,8 @@ public class StreamingController {
     @GetMapping(value = "/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<?> getStream() {
         return Flux.generate(() -> 0, (state, sink) -> {
-            sink.next("Lol " + state);
+            sink.next(new Message(LocalDateTime.now().toString()));
+            if (state == 1000) sink.complete();
             return state + 1;
         }).delayElements(Duration.ofSeconds(2));
     }
--- react/src/main/java/com/example/spring/react/service/ReactiveService.java
+++ react/src/main/java/com/example/spring/react/service/ReactiveService.java
@@ -1,23 +1,27 @@
 package com.example.spring.react.service;
 
 import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
 
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ReactiveService {
 
+//    http://www.baeldung.com/reactor-core
     public static void main(String[] args) {
         Flux<String> flux = Flux.just("One", "Two", "Three");
         Flux.range(5, 3)
                 .subscribe(System.out::println);
         Flux.fromIterable(Stream.of("One", "Two", "Three").collect(Collectors.toList()))
                 .subscribe(System.out::println);
-        Flux.range(1, 4)
-                .map(i -> {
-                    if (i <= 3) return i;
-                    throw new RuntimeException("Got to 4");
-                })
+        Flux.range(1, 40)
+//                .map(i -> {
+//                    if (i <= 3) return i;
+//                    throw new RuntimeException("Got to 4");
+//                })
+//                .publishOn(Schedulers.parallel())
+//                .subscribeOn(Schedulers.parallel(), )
                 .subscribe(System.out::println,
                         error -> System.err.println("Error: " + error),
                         () -> System.out.println("Done"));
